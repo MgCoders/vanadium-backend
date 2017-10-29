9604da608f27b78b96e7ab6c1b2aa815edfb438e
rsperoni <rsperoni@mgcoders.com>
2017-10-29 09:04:00 -0300

    amazon

---
+++ download_conf.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+aws s3 cp ... /home/ubuntu/sulfur-backend-deploy/.env
--- start_server.sh
+++ start_server.sh
@@ -3,7 +3,6 @@
 set -x
 echo Logging in to Amazon ECR...
 $(aws ecr get-login --region $AWS_DEFAULT_REGION)
-echo cd omicflows-backend-deploy en home
-cd /home/ubuntu/omicflows-backend-deploy
-echo docker-compose up
+cp /home/ubuntu/conf/sulfur-backend-deploy.env /home/ubuntu/sulfur-backend-deploy/.env
+cd /home/ubuntu/sulfur-backend-deploy
 docker-compose -f docker-compose.production.yml build && docker-compose -f docker-compose.production.yml up -d wildfly
