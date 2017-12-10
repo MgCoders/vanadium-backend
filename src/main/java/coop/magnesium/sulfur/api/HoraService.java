7aa49c6687c1abe4e11906eecf1537a85a4fdd0b
rsperoni <rsperoni@mgcoders.com>
2017-12-10 15:22:58 -0300

    Serialización horas.

--- src/main/java/coop/magnesium/sulfur/api/HoraService.java
+++ src/main/java/coop/magnesium/sulfur/api/HoraService.java
@@ -106,8 +106,10 @@ public class HoraService {
     @ApiOperation(value = "Get hora", response = Hora.class)
     @ApiResponses(value = {
             @ApiResponse(code = 404, message = "Id no encontrado")})
-    public Hora find(@PathParam("id") Long id) {
-        return horaDao.findById(id);
+    public Response find(@PathParam("id") Long id) {
+        Hora hora = horaDao.findById(id);
+        if (hora == null) return Response.status(Response.Status.NOT_FOUND).build();
+        return Response.ok(hora).build();
     }
 
     @PUT
