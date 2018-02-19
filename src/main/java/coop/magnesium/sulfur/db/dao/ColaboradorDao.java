7b4aeabd3cec46b1926951752c49437e6ee79a81
rsperoni <rsperoni@mgcoders.com>
2018-02-19 18:01:08 -0300

    Obtener Colaboradores por Cargo

--- src/main/java/coop/magnesium/sulfur/db/dao/ColaboradorDao.java
+++ src/main/java/coop/magnesium/sulfur/db/dao/ColaboradorDao.java
@@ -1,11 +1,17 @@
 package coop.magnesium.sulfur.db.dao;
 
+import coop.magnesium.sulfur.db.entities.Cargo;
 import coop.magnesium.sulfur.db.entities.Colaborador;
 import coop.magnesium.sulfur.utils.ex.MagnesiumBdMultipleResultsException;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 /**
@@ -33,5 +39,17 @@ public class ColaboradorDao extends AbstractDao<Colaborador, Long> {
         if (result.size() > 1)
             throw new MagnesiumBdMultipleResultsException(Colaborador.class.getSimpleName() + "multiples resultados encontrados");
         return result.get(0);
+    }
+
+    public List<Colaborador> findAllByCargo(Cargo cargo) {
+        CriteriaBuilder criteriaBuilder = this.getEntityManager().getCriteriaBuilder();
+        CriteriaQuery criteriaQuery = criteriaBuilder.createQuery();
+        Root entity = criteriaQuery.from(Colaborador.class);
+        criteriaQuery.select(entity);
+        Predicate cargoIsOk = criteriaBuilder.equal(entity.get("cargo"), criteriaBuilder.parameter(Cargo.class, "c"));
+        criteriaQuery.where(cargoIsOk);
+        Query query = this.getEntityManager().createQuery(criteriaQuery);
+        query.setParameter("c", cargo);
+        return (List<Colaborador>) query.getResultList();
     }
 }
