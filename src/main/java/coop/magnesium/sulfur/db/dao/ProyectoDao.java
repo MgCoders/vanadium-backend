c839f492172eee45b6dcfe6e321e32fb705c7b0a
rsperoni <rsperoni@mgcoders.com>
2018-02-19 17:31:42 -0300

    Obtener proyectos por prioridad.

--- src/main/java/coop/magnesium/sulfur/db/dao/ProyectoDao.java
+++ src/main/java/coop/magnesium/sulfur/db/dao/ProyectoDao.java
@@ -5,6 +5,11 @@ import coop.magnesium.sulfur.db.entities.Proyecto;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
 
 /**
  * Created by rsperoni on 28/10/17.
@@ -23,5 +28,15 @@ public class ProyectoDao extends AbstractDao<Proyecto, Long> {
     @Override
     public EntityManager getEntityManager() {
         return em;
+    }
+
+    public List<Proyecto> findAllByPrioridad() {
+        CriteriaBuilder criteriaBuilder = this.getEntityManager().getCriteriaBuilder();
+        CriteriaQuery criteriaQuery = criteriaBuilder.createQuery();
+        Root entity = criteriaQuery.from(Proyecto.class);
+        criteriaQuery.select(entity);
+        criteriaQuery.orderBy(criteriaBuilder.desc(entity.get("prioridad")));
+        Query query = this.getEntityManager().createQuery(criteriaQuery);
+        return (List<Proyecto>) query.getResultList();
     }
 }
