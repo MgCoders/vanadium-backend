d483af350dfd61ad2f62365e61cd7c786398bea3
rsperoni <rsperoni@mgcoders.com>
2018-04-02 18:36:11 -0300

    javaMail

--- src/main/java/coop/magnesium/sulfur/system/MailService.java
+++ src/main/java/coop/magnesium/sulfur/system/MailService.java
@@ -72,21 +72,21 @@ public class MailService {
     public void init() {
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
+        props.setProperty("mail.pop3.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.setProperty("mail.smtp.socketFactory.fallback", "false");
+        props.setProperty("mail.smtp.socketFactory.port", configuracionDao.getMailPort());
+        props.put("mail.smtp.startssl.enable", "true");
         props.put("mail.smtp.host", configuracionDao.getMailHost());
         props.put("mail.smtp.port", configuracionDao.getMailPort());
 
-        if (configuracionDao.getMailPass()!=null) {
-            mailSession = Session.getInstance(props,
-                    new javax.mail.Authenticator() {
-                        protected PasswordAuthentication getPasswordAuthentication() {
-                            return new PasswordAuthentication(configuracionDao.getMailFrom(), configuracionDao.getMailPass());
-                        }
+        mailSession = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(configuracionDao.getMailFrom(), configuracionDao.getMailPass());
                     }
-            );
-        } else {
-            mailSession = Session.getInstance(props);
-        }
+                }
+        );
     }
 
     @Logged
